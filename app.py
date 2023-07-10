fe346a8e1804867f42ba99a5dc0f85a6a58d2542
online-code-bite-md-tanvir-mahtab <73059977+online-code-bite-md-tanvir-mahtab@users.noreply.github.com>
2023-07-10 17:45:25 +0100

    Update app.py
    i just fixed the code

--- app.py
+++ app.py
@@ -1,139 +1,79 @@
-class GroundScrew:
-    def __init__(self, length, weight):
-        self.length = length
-        self.weight = weight
-
-
-class ShippingMethod:
-    def __init__(self, name, max_length, max_weight, fixed_cost=None):
-        self.name = name
-        self.max_length = max_length
-        self.max_weight = max_weight
-        self.fixed_cost = fixed_cost
-
-
-class TGVLogisticsMethod(ShippingMethod):
-    def __init__(self, name, variable_costs_by_distance):
-        super().__init__(name, None, None)
-        self.variable_costs_by_distance = variable_costs_by_distance
-
-
-class ShippingCalculator:
-    def __init__(self, products, shipping_methods):
-        self.products = products
-        self.shipping_methods = shipping_methods
-
-    def calculate_shipping_costs(self):
-        cheapest_method = None
-        cheapest_cost = float('inf')
-        packages_count = {method.name: 0 for method in self.shipping_methods}
-        
-        for method in self.shipping_methods:
-            total_cost = 5
-            for product in self.products.values():
-                if self.is_product_within_limits(product, method):
-                    if method.fixed_cost is not None:
-                        total_cost += method.fixed_cost
-                    elif isinstance(method, TGVLogisticsMethod):
-                        distance = self.calculate_distance()  # Implement distance calculation logic
-                        cost_per_km = self.get_variable_cost_by_distance(method, distance)
-                        total_cost += cost_per_km * distance
-                    packages_count[method.name] += 1
-
-            if total_cost < cheapest_cost:
-                cheapest_cost = total_cost
-                cheapest_method = method
-
-        return cheapest_method, packages_count, cheapest_cost
-    
-
-
-    def is_product_within_limits(self, product, method):
-        if method.max_length and product.length > method.max_length:
-            return False
-        if method.max_weight and product.weight > method.max_weight:
-            return False
-        return True
-
-    def get_variable_cost_by_distance(self, method, distance):
-        for dist, cost in method.variable_costs_by_distance.items():
-            if distance <= dist:
-                return cost
-        return cost
-
-    def calculate_distance(self):
-        customer_postcode = '900'
-        your_postcode = "0"  # Replace with your address postcode
-
-        # Assume postcodes are numeric and have the same length
-        distance = abs(int(customer_postcode) - int(your_postcode))
-        return distance
-
-
-
-
-
-
-shipping_methods_data = {
-    "PostNL Regular Package": ShippingMethod("PostNL Regular Package", 100, 23, fixed_cost=10.0),
-    "PostNL Large Package": ShippingMethod("PostNL Large Package", 175, 30, fixed_cost=15.0),
-    "TGV Logistics": TGVLogisticsMethod("TGV Logistics", 
-                                        variable_costs_by_distance={
-                                            50: 12.0,
-                                            100: 18.0,
-                                            150: 24.0
-                                        }
-                                    ),
-}
-
 from kivymd.app import MDApp
-from kivy.lang import Builder
-from kivymd.uix.screen import MDScreen
-from kivymd.uix.button import MDFloatingActionButton, MDFillRoundFlatButton
-from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFillRoundFlatButton, MDFloatingActionButton
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.boxlayout import BoxLayout
+
 from kivymd.uix.textfield import MDTextField
-from app import *
 
-from kivymd.uix.dialog import MDDialog
+class HomeScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
+        layout = BoxLayout(orientation="vertical", spacing=20, padding=40)
 
-
-
-
-class ShippingApp(MDApp):
-    
-    def add_data_release(self, instance):
-        self.products_data = {}
-        self.quentiry = int(self.product_quentity.text)
-        for _ in range(self.quentiry):
-            name = self.product_name.text
-            max_length = int(self.product_length.text)
-            max_weight = int(self.product_weight.text)
-            self.products_data[name] = GroundScrew(length=max_length,weight=max_weight)
-        self.calculate.icon = 'equal'
-    
-    
-    def on_fab_release(self, instance):
-        # Create instances of the classes
-        products = {name: product for name, product in self.products_data.items()}
-        shipping_methods = [method for name, method in shipping_methods_data.items()]
-        # Create the ShippingCalculator instance
-        shipping_calculator = ShippingCalculator(products, shipping_methods)
-
-        # Calculate the shipping costs
-        cheapest_method, packages_count, cheapest_cost = shipping_calculator.calculate_shipping_costs()
-        cheapest_cost = cheapest_cost * self.quentiry
-        text= f'Cheapest Shipping Method: {cheapest_method.name}\n Package Count: {packages_count}\n Total Cost: {cheapest_cost}'
-        dialog = MDDialog(
-            title="Dialog Box",
-            text=text,
-            size_hint=(0.8, 0.4),
+        add_product_button = MDFillRoundFlatButton(
+            text="Add/remove product",
+            size_hint=(0.5, None),
+            height="48dp",
+            md_bg_color=(0.3, 0.3, 0.3, 1),
+            on_release=self.add_product_release
         )
-        dialog.open()
 
-    def build(self):
-        self.theme_cls.theme_style = "Light"
-        screen = MDScreen()
+        shipping_method_button = MDFillRoundFlatButton(
+            text="Shipping Method",
+            size_hint=(0.5, None),
+            height="48dp",
+            md_bg_color=(0.3, 0.3, 0.3, 1),
+            on_release=self.shipping_method_release
+        )
+
+        calculate_shipping_button = MDFillRoundFlatButton(
+            text="Calculate Shipping",
+            size_hint=(0.5, None),
+            height="55dp",
+            md_bg_color=(0.3, 0.3, 0.3, 1),
+            on_release=self.calculate_shipping_release
+        )
+        layout.pos_hint = {
+            "center_y": 0.9,
+            "center_x": 0.75
+        }
+        layout.add_widget(add_product_button)
+        layout.add_widget(shipping_method_button)
+        layout.add_widget(calculate_shipping_button)
+
+        self.add_widget(layout)
+
+    def add_product_release(self, instance):
+        # Handle button release event for Add Product
+
+        # Switch to the AddProductScreen
+        self.manager.current = "add_product"
+
+    def shipping_method_release(self, instance):
+        # Handle button release event for Shipping Method
+
+        # Switch to the ShippingMethodScreen
+        self.manager.current = "shipping_method"
+
+    def calculate_shipping_release(self, instance):
+        # Handle button release event for Calculate Shipping
+
+        # Switch to the CalculateShippingScreen
+        self.manager.current = "calculate_shipping"
+
+
+class AddProductScreen(Screen):
+    def button_release(self, instance):
+        # Handle button release event
+        print('hello world')
+        self.manager.current = "home"
+
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        layout = BoxLayout(orientation="vertical", spacing=10, padding=40)
         self.product_quentity = MDTextField(
             hint_text="How many Product You want to store",
             helper_text="Please enter numerical value",
@@ -171,31 +111,107 @@ class ShippingApp(MDApp):
             width= 300
         )
         self.add_button = MDFillRoundFlatButton(
-            text="Add To Data",
+            text="Square Button",
             pos_hint={"center_x": 0.5, "center_y": 0.40},
-            size_hint=(0.2, 0.1),
+            # size_hint=(0.2, 0.1),
             md_bg_color=(0.3, 0.3, 0.3, 1),  
-            on_release=self.add_data_release
+            # on_release=self.add_data_release
         )
 
 
-        self.calculate = MDFloatingActionButton(
-            icon="plus",
+        self.calculate = MDFillRoundFlatButton(
+            icon="Delete",
             pos_hint={"right": 0.95, "y": 0.1},
-            on_release=self.on_fab_release
+            # on_release=self.on_fab_release
         )
 
 
-        screen.add_widget(self.product_quentity)
-        screen.add_widget(self.product_name)
-        screen.add_widget(self.product_length)
-        screen.add_widget(self.product_weight)
-        screen.add_widget(self.add_button)
-        screen.add_widget(self.calculate)
-        return screen
+        
+        button = MDFloatingActionButton(
+            text="Button with Icon",
+            icon="home",  # Replace with the desired icon name
+            pos_hint={"center_x": 0.9, "center_y": 0.5},
+            # size_hint=(0.2, 0.1),
+            md_bg_color=(0.3, 0.3, 0.3, 1),
+            on_release=self.button_release
+        )
+        layout.add_widget(self.product_quentity)
+        layout.add_widget(self.product_name)
+        layout.add_widget(self.product_length)
+        layout.add_widget(self.product_weight)
+        layout.add_widget(self.add_button)
+        layout.add_widget(self.calculate)
+        layout.add_widget(button)
+        # Add widgets specific to AddProductScreen
+
+        self.add_widget(layout)
 
 
+class ShippingMethodScreen(Screen):
+    def button_release(self, instance):
+        # Handle button release event
+        print('hello world')
+        self.manager.current = "home"
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        layout = BoxLayout(orientation="vertical", spacing=10, padding=40)
+
+        # Add widgets specific to ShippingMethodScreen
+        button = MDFloatingActionButton(
+            text="Button with Icon",
+            icon="home",  # Replace with the desired icon name
+            pos_hint={"center_x": 0.9, "center_y": 0.5},
+            # size_hint=(0.2, 0.1),
+            md_bg_color=(0.3, 0.3, 0.3, 1),
+            on_release=self.button_release
+        )
+
+        layout.add_widget(button)
+        self.add_widget(layout)
 
 
-if __name__ == "__main__":
-    ShippingApp().run()
+class CalculateShippingScreen(Screen):
+    def button_release(self, instance):
+        # Handle button release event
+        print('hello world')
+        self.manager.current = "home"
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        layout = BoxLayout(orientation="vertical", spacing=10, padding=40)
+
+        # Add widgets specific to CalculateShippingScreen
+        button = MDFloatingActionButton(
+            text="Button with Icon",
+            icon="home",  # Replace with the desired icon name
+            pos_hint={"center_x": 0.9, "center_y": 0.5},
+            # size_hint=(0.2, 0.1),
+            md_bg_color=(0.3, 0.3, 0.3, 1),
+            on_release=self.button_release
+        )
+
+        layout.add_widget(button)
+        self.add_widget(layout)
+
+
+class MyApp(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Light"
+
+        screen_manager = ScreenManager()
+
+        home_screen = HomeScreen(name="home")
+        add_product_screen = AddProductScreen(name="add_product")
+        shipping_method_screen = ShippingMethodScreen(name="shipping_method")
+        calculate_shipping_screen = CalculateShippingScreen(name="calculate_shipping")
+
+        screen_manager.add_widget(home_screen)
+        screen_manager.add_widget(add_product_screen)
+        screen_manager.add_widget(shipping_method_screen)
+        screen_manager.add_widget(calculate_shipping_screen)
+
+        return screen_manager
+
+
+MyApp().run()
